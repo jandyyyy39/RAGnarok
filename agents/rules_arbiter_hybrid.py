a8cc6b6984406b53c58a7a43e2c1ca9bf894fd55
AndresFernandoGarcia <andresgercie@gmail.com>
2026-03-20 13:37:27 +0800

    Edit hybrid __init__  method to work with MY established bm25 index and chunks

--- agents/rules_arbiter_hybrid.py
+++ agents/rules_arbiter_hybrid.py
@@ -29,9 +29,9 @@ class RulesArbiterHybrid:
         # BM25 keyword index (loaded from the pickle you built in Step 4)
         with open(Config.DATA_DIR / 'bm25_index.pkl', 'rb') as f:
             self.bm25 = pickle.load(f)
-        with open(Config.DATA_DIR / 'chunks_raw.pkl', 'rb') as f:
-            data = pickle.load(f)
-            self.chunk_texts = data['texts']
+        with open(Config.DATA_DIR / 'bm25_chunks.pkl', 'rb') as f:
+            final_splits = pickle.load(f)
+            self.chunk_texts = [doc.page_content for doc in final_splits]
 
         # Cross-encoder re-ranker
         self.reranker = CrossEncoder(
