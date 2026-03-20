51b4fcd7bad5d35e3a75cb542d5d6a0ae6baa7dc
AndresFernandoGarcia <andresgercie@gmail.com>
2026-03-20 13:43:36 +0800

    Fix imports

--- evaluation/evaluate_retrieval_header.py
+++ evaluation/evaluate_retrieval_header.py
@@ -8,7 +8,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from test_rag import (
+from scripts.test_rag import (
     test_retrieval_cosine_sim,
     test_bm25,
     retrieve_with_rerank,
